4a6e108c40ef1a610b3c9898712acf1b3218c2ce
Haibo Zeng <52969584+haibo-zeng@users.noreply.github.com>
2022-02-10 12:08:12 -0500

    Initial commit

---
+++ spring22-assignment05.cpp
@@ -0,0 +1,14 @@
+// TODO: Comments should be used to identify the course, semester, program's purpose and author.
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	// TODO: Your code goes here, consistently indented one tab position relative to the braces.
+	// TODO: The last line should be "return 0;" since main() is a function that returns an integer status.
+	// TODO: Returning 0 means the program ended normally.
+}
